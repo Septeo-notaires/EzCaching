666929e9045bfc5991a93e78ce9a960826cb018b
Damien Venant-Valéry <damien.venantvalery@septeo.com>
2025-04-19 13:39:00 +0200

    feat: add cache

--- src/EzCache.Console/Program.cs
+++ src/EzCache.Console/Program.cs
@@ -1,7 +1,55 @@
-﻿public class Program
+﻿using EzCache.Cache;
+
+public class Program
 {
+    private static readonly LruCache cache = new LruCache(10);
     public static void Main()
     {
 
+        while (true) {
+            Console.Clear();
+            Console.WriteLine("Caching front");
+            Console.WriteLine("1. Add in cache");
+            Console.WriteLine("2. Get in cache");
+            Console.WriteLine("3. quit");
+
+            string? id = Console.ReadLine();
+
+            Action act = id switch
+            {
+                "1" => AddInCache,
+                "2" => GetInCache,
+                "3" => Quit,
+                _ => Quit
+            };
+
+            act();
+        }
     }
+
+    private static void AddInCache()
+    {
+        Console.Clear();
+        Console.Write("Key : ");
+        string? key = Console.ReadLine();
+        Console.Write("Value : ");
+        string? value = Console.ReadLine();
+
+        cache.Add(key, value);
+    }
+
+    private static void GetInCache()
+    {
+        Console.Clear();
+        Console.Write("Key : ");
+        string? key = Console.ReadLine();
+
+        string? val = cache.GetElement(key) as string;
+
+        Console.WriteLine(val);
+        _ = Console.ReadKey();
+    }
+
+    private static void Quit() =>
+        Environment.Exit(0);
 }
---
+++ src/EzCache/Cache/LruCache.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace EzCache.Cache
+{
+    public class LruCache
+    {
+        #region Private Variables
+        private readonly int _capacity;
+        private readonly Mutex _mt = new Mutex();
+
+        private int _lenght;
+        private Dictionary<string, LinkedListNode<ObjectValue>> _fastAccess = new Dictionary<string, LinkedListNode<ObjectValue>>();
+        private LinkedList<ObjectValue> _cache = new LinkedList<ObjectValue>();
+        #endregion Private Variables
+
+        #region Properties 
+        #endregion Properties
+
+        struct ObjectValue
+        {
+            public string Key { get; private set; }
+            public object Value { get; private set; }
+
+            public ObjectValue(string key, object value)
+            {
+                Key = key;
+                Value = value;
+            }
+        }
+
+        public LruCache(int capacity) => 
+            _capacity = capacity;
+
+        public void Add(string key, object value)
+        {
+            _mt.WaitOne();
+            if (_lenght >= _capacity) RemoveLeastUsed();
+            if (_fastAccess.ContainsKey(key))
+            {
+                _mt.ReleaseMutex();
+                throw new InvalidOperationException("");
+            }
+
+            LinkedListNode<ObjectValue> node = _cache.AddFirst(new ObjectValue(key, value));
+            _fastAccess.Add(key, node);
+            _lenght++;
+            _mt.ReleaseMutex();
+        }
+
+        public void Remove(string key)
+        {
+            _mt.WaitOne();
+            if (!_fastAccess.ContainsKey(key))
+            {
+                _mt.ReleaseMutex();
+                throw new InvalidOperationException("");
+            }
+            LinkedListNode<ObjectValue> val = _fastAccess[key];
+            _cache.Remove(val);
+            _fastAccess.Remove(val.Value.Key);
+            --_lenght;
+            _mt.ReleaseMutex();
+        }
+
+        public object GetElement(string key)
+        {
+            _mt.WaitOne();
+            if (_fastAccess.ContainsKey(key))
+            {
+                LinkedListNode<ObjectValue> node =_fastAccess[key];
+                _cache.Remove(node);
+                _fastAccess[key] = _cache.AddFirst(node.Value);
+                _mt.ReleaseMutex();
+                return node.Value.Value;
+            }
+            _mt.ReleaseMutex();
+            return null;
+        }
+
+        private void RemoveLeastUsed()
+        {
+            LinkedListNode<ObjectValue> lastNode = _cache.Last;
+            --_lenght;
+            _cache.Remove(lastNode);
+            _fastAccess.Remove(lastNode.Value.Key);
+        }
+    }
+}
